219b936973c5d769f92a8d7d31f5ae8c590c15b4
zach pratt <zach@attackofzach.com>
2014-09-06 23:35:53 -0500

    Updated flow for loading the google maps API and initializing the map.

--- jsx/index.jsx
+++ jsx/index.jsx
@@ -9,20 +9,17 @@
         React = require('react'),
         Router = require('./lib/router.coffee'),
         OverlayView = require('./lib/overlay-component'),
+        loadMap = require('./lib/load-map');
 
-        loadMap = require('./lib/load-map'),
-        mapLoaded;
+    loadMap.load();
+    loadMap.initMap().done(function (map){
+        var MapOverlay = require('./lib/map-overlay'),
+            overlayPoint = new google.maps.LatLng(40.01144663490021, -90.22767623046876),
+            overlayElement = document.createElement('div'),
+            mapOverlayView = new MapOverlay(overlayElement, overlayPoint);
 
-    mapLoaded = $(document).ready(loadMap.load);
-
-    //mapLoaded.done().then(function (map) {
-    //    var MapOverlay = require('./lib/map-overlay'),
-    //
-    //        overlayPoint = new google.maps.LatLng(40.01144663490021, -90.22767623046876),
-    //        overlayElement = document.createElement('div'),
-    //        mapOverlayView = new MapOverlay(overlayElement, overlayPoint);
-    //
-    //    React.renderComponent(<OverlayView phrase={'from a rendered component'} />, overlayElement);
-    //    mapOverlayView.setMap(map);
-    //});
+        overlayElement.className = 'map-overlay';
+        React.renderComponent(<OverlayView phrase={'from a rendered component'} />, overlayElement);
+        mapOverlayView.setMap(map);
+    });
 }());
